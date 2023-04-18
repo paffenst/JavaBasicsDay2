b5f8fd542fba7179752cc3e8ddd739fbb2e59a46
paffen <pavel.stoyanov0@hm.edu>
2023-04-18 22:39:45 +0200

    add for loop fakultat

---
+++ src/fakultaetFor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class fakultaetFor {
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte eine Zahl eingeben: ");
+        int eins = Integer.parseInt(scanner.next());
+        berechneFak(eins);
+    }
+    static void berechneFak(int zahl) {
+        int fak = 1;
+        for (int i = 1; i <= zahl; ++i) {
+            fak = fak * i;
+        }
+        System.out.println("Die Fakultät von " + zahl + " ist: " + fak);
+    }
+}
