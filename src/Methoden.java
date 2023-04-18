6fdb506cb0fafd09af26dc9818a5fb97f3059d93
paffen <pavel.stoyanov0@hm.edu>
2023-04-18 19:29:42 +0200

    add some code

--- src/Methoden.java
+++ src/Methoden.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Methoden {
     public static void main(String[] args) {
-        
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Wort eingeben: ");
+    String wort = scanner.next();
+        length(wort);
+    }
+    public static void length(String word){
+        if(word.length() > 20 ){
+            System.out.println("Ihr Wort ist länger als 20 Zeichen.");
+            include_fency(word);
+        } else if (word.length() < 20 ) {
+            System.out.println("Ihr Wort ist kürzer als 20 Zeichen.");
+            include_fency(word);
+        }else {
+            System.out.println("Ihr Wort besteht genau aus 20 Zeichen!");
+            include_fency(word);
+        }
+    }
+    public static void include_fency(String fency){
+        if(fency.contains("fency") ){
+            System.out.println("Ihr Wort enthählt das Wort 'fency' ");
+        }
     }
 }
